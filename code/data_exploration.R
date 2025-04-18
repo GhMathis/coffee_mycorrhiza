20d7b703a8525ba1d44f5e2676f9c7f9e1374671
Mathis.g <mathisgheno@gmail.com>
2025-04-18 19:52:57 +0200

    :sparkles: start test hmsc species level

--- code/data_exploration.R
+++ code/data_exploration.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 data_18S <- "data/18S_AMf_PR2/Robust_MiSeq_18S_AMf_20241120_125_samples.OTU.filtered.cleaved.nosubstringOTUs.mumu.table2"
+
 amf_taxonomic_levels <- c("kingdom", "domain", "phylum", "class", "order", "family",
                           "genus", "species")
 
@@ -17,36 +18,36 @@ amf_OTUs <- data_18S %>%
   rename_with(~sub("ROB_T_", "CONT_ROB_", .)) %>%
   rename_with(~sub("ROB_Tex", "CONT_ROB_T_ex", .)) %>%
   #{{select samples from ROBUST project}}
-  select(OTU, taxonomy, starts_with("CONT", ignore.case = FALSE),
+  select(amplicon, taxonomy, starts_with("CONT", ignore.case = FALSE),
          starts_with("ROB", ignore.case = FALSE)) %>%
   #{{"Arbuscular mycorrhizal fungi & Mucoromycotina fine root endophytes" are selected}}
   filter(str_detect(taxonomy,"Glomeromycotina")|str_detect(taxonomy,"Endogonales")) %>%
   #{{select only OTUs and controls}}
-  select(OTU, starts_with("CONT", ignore.case = FALSE),
+  select(amplicon, starts_with("CONT", ignore.case = FALSE),
          starts_with("ROB_", ignore.case = FALSE)) %>%
   droplevels()
-
+amf_OTUs
 taxonomy <- data_18S %>%
   #{{import data}}
   read_tsv() %>%
   filter(str_detect(taxonomy,"Glomeromycotina")|str_detect(taxonomy,"Endogonales")) %>%
-  select(OTU, taxonomy) %>%
+  select(amplicon, taxonomy) %>%
   mutate(taxonomy = str_replace_all(taxonomy, ":","_"),
-         taxonomy = str_replace_all(taxonomy, "[|]",";"))
+         taxonomy = str_replace_all(taxonomy, "[|]","; "))
 
 
 d <- amf_OTUs %>% 
   replace(amf_OTUs == 0, NA) %>%
-  select(OTU, starts_with("CONT")) %>%
-  pivot_longer(-OTU, names_to = "samples", values_to = "reads") %>%
+  select(amplicon, starts_with("CONT")) %>%
+  pivot_longer(-amplicon, names_to = "samples", values_to = "reads") %>%
   filter(!is.na(reads))
 
 amf_OTUs_decont <- amf_OTUs %>%
   replace(amf_OTUs == 0, NA) %>%
-  pivot_longer(-c("OTU", starts_with("CONT")), names_to = "samples", values_to = "reads") %>%
+  pivot_longer(-c("amplicon", starts_with("CONT")), names_to = "samples", values_to = "reads") %>%
   filter(!is.na(reads)) %>%
   #{{merge with control samples}}
-  left_join(count(d, OTU, wt = reads), by = "OTU") %>%
+  left_join(count(d, amplicon, wt = reads), by = "amplicon") %>%
   #{{substract abundance of control samples}}
   mutate(reads = case_when(
     is.na(n)  ~ reads,
@@ -55,7 +56,7 @@ amf_OTUs_decont <- amf_OTUs %>%
   select(-n,-starts_with("CONT")) %>%
   pivot_wider(names_from = samples, values_from =  reads, values_fill = 0) %>%
   as_tibble() %>%
-  column_to_rownames(var = "OTU") 
+  column_to_rownames(var = "amplicon") 
 colSums(amf_OTUs_decont)
 
 # rarefaction
@@ -202,11 +203,80 @@ diversities_afm_df %>%
 # BiocManager::install("Biostrings")
 # devtools::install_github('reptalex/phylofactor')
 library(phylofactor)
-
-tree_fr
-PF <- PhyloFactor(Data = amf_OTUs_rare,X = soil_df$coffee_system, nfactors=3)
+library(ape)
 
 
+
+
+tree_amf <- read.tree(file = "data/18S_AMf_PR2/Robust_MiSeq_18S_AMf_20241120_125_samples_Glomeromycotina.tree")
+dim(amf_OTUs_decont2)
+pf.heatmap(tree=tree_amf,Data=log1p(amf_OTUs_decont2),color = NA)
+pf.heatmap(tree=tree_amf,Data=log1p(amf_OTUs_rare))
+rownames(amf_OTUs_rare) %in% tree_amf$tip.label
+
+plot(tree_amf,show.tip.label = F)
+
+soil_df %>% 
+  filter(soil_code%in% colnames(amf_OTUs_rare))%>%
+  mutate(coffee_system = as.factor(coffee_system)) %>%
+  column_to_rownames("soil_code")-> soil_df2
+amf_OTUs_rare%>%
+  select(-c(ROB_M_s, ROB_L_s)) -> Data_abund
+Data_abund = as.matrix(Data_abund[tree_amf$tip.label,])
+
+t(t(amf_OTUs_decont2 )/colSums(amf_OTUs_decont2)) %>%
+  as.data.frame %>%
+  select(-c(ROB_M_s, ROB_L_s)) -> amf_OTUs_relative
+
+soil_df2 %>%
+  arrange(coffee_system) %>%
+  rownames -> arrange_sample
+amf_OTUs_relative = as.matrix(amf_OTUs_relative[tree_amf$tip.label,arrange_sample])
+
+soil_df2$coffee_system %>% unique
+temp_amf_OTUs_relative <- amf_OTUs_relative
+
+colnames(temp_amf_OTUs_relative) <- soil_df2 %>%
+  arrange(coffee_system) %>%
+  pull(coffee_system)
+
+PF <- PhyloFactor(temp_amf_OTUs_relative,tree_amf,soil_df2$coffee_system,nfactors=3)
+library(phytools)
+clr <- function(Matrix) apply(Matrix,MARGIN=2,FUN=function(x) log(x)-mean(log(x)))
+phylo.heatmap(tree_amf,clr(PF$Data), ftype = "off")
+pf.heatmap(PF,factors=1:3)
+
+bin.projection <- pf.BINprojection(PF,factor=3)   
+bin.taxa <- bin.projection$otus %>% 
+  lapply(.,FUN=function(otus,tax) tax[otus,],tax=as.data.frame(taxonomy) %>% column_to_rownames("amplicon"))
+
+PF$nfactors
+bin.observed.geometricMeans <- pf.BINprojection(PF)$Data
+bin.observed.relativeAbundances <- pf.BINprojection(PF,rel.abund=T)$Data
+bin.predicted.geometricMeans <- pf.BINprojection(PF,prediction=T)$Data
+bin.predicted.relativeAbundances <- pf.BINprojection(PF,prediction=T,rel.abund=T)$Data
+
+library(plotrix)
+par(mfrow=c(2,2))
+stackpoly(t(bin.observed.geometricMeans),stack=T,main='Observed gMean',xat = c(18,28,46,66,84),xaxlab = c("RCB", "RCM", "RCT", "RCTB", "RCTBA"))
+stackpoly(t(bin.predicted.geometricMeans),stack=T,main='Predicted gMean',xat = c(18,28,46,66,84),xaxlab = c("RCB", "RCM", "RCT", "RCTB", "RCTBA"))
+
+stackpoly(t(bin.observed.relativeAbundances),stack=T,main='Observed Rel-Abund.',xat = c(18,28,46,66,84),xaxlab = c("RCB", "RCM", "RCT", "RCTB", "RCTBA"))
+stackpoly(t(bin.predicted.relativeAbundances),stack=T,main='Predicted Rel-Abund.',xat = c(18,28,46,66,84),xaxlab = c("RCB", "RCM", "RCT", "RCTB", "RCTBA"))
+par(mfrow=c(1,1))
+prediction <- pf.predict(PF,factors=3)
+phylo.heatmap(tree_amf,clr(prediction), ftype = "off")
+# PA_datatable <- matrix.to.phyloframe(Data_abund,soil_df2$coffee_system,data.name = 'Abundance')
+# PF <- gpf(Data = Data_abund, 
+#           tree =tree_amf, MetaData = soil_df2,
+#           frmla.phylo = Data~phylo*coffee_system,
+#           family = poisson,
+#           nfactors=3, algorithm='mStable')
+# PF
+phyca <- PhyCA(Data_abund,tree_amf,ncores=8,ncomponents = 5)
+
+pf.heatmap(PF,factors=1:3)
+summary(Phylofactorization)
 ### test Hmsc
 
 library(Hmsc)
@@ -227,21 +297,67 @@ if (test.run){
   transient = 500*thin
   verbose = 500*thin
 }
-
+colSums(Y) %>% sort
 Y = as.matrix(amf_OTUs_rare %>%
                 select(-c(ROB_M_s, ROB_L_s)) %>%
-                filter(rowSums(across(starts_with("ROB"))) > 100) %>%
+                filter(rowSums(across(starts_with("ROB"))) >=1) %>%
                 t())
 colSums(Y) %>% sort
 soil_df %>% 
   filter(soil_code%in% rownames(Y))%>%
-  select(coffee_system) %>%
-  mutate(coffee_system = as.factor(coffee_system))-> soil_df2
+  mutate(coffee_system = as.factor(coffee_system)) %>%
+  column_to_rownames("soil_code")-> soil_df2
 dim(Y)
 m1 = Hmsc(Y = Y, XData = soil_df2, XFormula = ~coffee_system)
 
 
 m_converg = sampleMcmc(m, thin = thin, samples = samples, transient = transient,
                nChains = nChains, verbose = verbose)
+####
+library(PLNmodels)
+str(Y)
+str(soil_df2)
+afm_data <- prepare_data(Y, soil_df2)
+myPLN <- PLN(Abundance ~ coffee_system , afm_data)
 
+plot(myPLN)
 
+library(FactoMineR)
+library(factoextra)
+PCA(Y) -> pca_amf
+fviz_pca_ind(pca_amf, col.ind = soil_df2$coffee_system)
+
+#####
+library(picante)
+
+library(vegan)
+
+rowSums(Data_abund)
+apply(Data_abund, 1, which.max) -> OTU_optima_index
+as.data.frame(OTU_optima_index) %>% 
+  mutate(OTU_optima_sample = colnames(Data_abund)[OTU_optima_index],
+         OTU_tot_abund = apply(Data_abund, 1, sum))%>%
+  filter(OTU_tot_abund !=0) %>%
+  rownames_to_column("OTU") %>%
+  left_join(soil_df2%>%
+              rownames_to_column("soil_code"), by = join_by("OTU_optima_sample" == "soil_code")) -> optima_df
+optima_df %>%
+  column_to_rownames("OTU") %>%
+  select(pH, N, P,K,Na,MO,sand,clay) %>%
+  dist -> dist_optima
+
+drop.tip(tree_amf, setdiff(tree_amf$tip.label, optima_df$OTU)) -> tree_amf_subset
+cophenetic(tree_amf_subset) -> amf_phylo_dist
+dist(Data_abund) -> afm_abund_dist
+str(dist_optima)
+str(amf_phylo_dist)
+vegan::mantel.correlog(dist_optima,amf_phylo_dist,n.class=50, nperm =999) -> mantel_phylo_optima
+plot(mantel_phylo_optima)
+vegan::mantel.correlog(afm_abund_dist,amf_phylo_dist,n.class=50, nperm =999) -> mantel_phylo
+plot(mantel_phylo)
+
+dist(t(amf_OTUs_relative)) -> afm_abund_rela_dist
+vegan::mantel.correlog(afm_abund_rela_dist,amf_phylo_dist,n.class=50, nperm =999) -> mantel_phylo_rela
+plot(mantel_phylo_rela)
+
+betaMNTD <- picante::comdistnt(amf_OTUs_relative, amf_phylo_dist, abundance.weighted = T)
